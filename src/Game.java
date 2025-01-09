7152560002802ff03c448541944338d413a364e6
Jacob Ketterer <jketterer02@gmail.com>
2025-01-09 07:06:20 -0600

    Fixing UI

--- src/Game.java
+++ src/Game.java
@@ -1,37 +1,36 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.*;
+import javax.swing.border.Border;
 
 public class Game
 {
-    int windowWidth = 600;
-    int windowHeight = 600;
-    int cardwidth = 50;
-    int cardheight = 75;
+    int windowWidth = 800;
+    int windowHeight = 800;
+    int cardwidth = 100;
+    int cardheight = 150;
+    int playerhandsize = 8;
+    int handcram = -5;
 
     ArrayList<Card> deck;
     ArrayList<Card> hand;
 
-
     JFrame gamewindow = new JFrame("Joker Poker");
     
-    JPanel gamebg = new JPanel()
-    {
-        @Override public void paintComponent(Graphics g)
-        {
-            super.paintComponent(g);
-            Image cardimg = new ImageIcon(getClass().getResource("./cards/errcard.PNG")).getImage();
-            g.drawImage(cardimg,20,20,cardwidth,cardheight,null);
-        }
-    };
-
+    JPanel gamebg = new JPanel();
+    JPanel handPanel = new JPanel();
     JPanel actionpanel = new JPanel();
+
+    ArrayList<JButton> buttonlist;
+    //There's interesting tech here with intializing all these buttons into an arraylist and iterating inside buildActionPanel
     JButton discardbtn = new JButton("Discard");
     JButton ranksortbtn = new JButton("Sort (Rank)");
     JButton suitsortbtn = new JButton("Sort (Suit)");
     JButton playbutton = new JButton("Play Hand");
+    
     
     private class Card
     {
@@ -45,7 +44,10 @@ public class Game
         }
 
         // Overload toString method
-        public String toString() { return rank + " of " + suit ; }
+        public String toString()
+        { 
+            return rank + " of " + suit ;
+        }
 
         public int getValue()
         {
@@ -57,6 +59,27 @@ public class Game
                 case "J": return 10;
 
                 default: return Integer.parseInt(rank);
+            }
+        }
+
+        public String getImagePath()
+        {
+            //String imagePath = "./cards/" + toString().replaceAll("\\s", "").toLowerCase() + ".png";
+            
+            //return "./cards/" + toString().replaceAll("\\s", "").toLowerCase() + ".png";
+
+            //return new File(("./cards/" + toString().replaceAll("\\s", "").toLowerCase() + ".png").isFile())
+            //? "./cards/" + toString().replaceAll("\\s", "").toLowerCase() + ".png"
+            //: "./cards/errcard.PNG";
+
+            String path = "./cards/" + toString().replaceAll("\\s", "").toLowerCase() + ".png";
+            if (new File(path).isFile())
+            {
+                return path;
+            } 
+            else
+            {
+                return "./cards/errcard.PNG";
             }
         }
     }
@@ -79,8 +102,8 @@ public class Game
     {
         deck = new ArrayList<Card>();
 
-        String[] suits = {"C", "D", "H", "S"};
-        String[] ranks = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+        String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
+        String[] ranks = {"Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"};
 
         for(int i=0; i<suits.length; i++)
         {
@@ -114,32 +137,6 @@ public class Game
 
     }
 
-    public void drawHand()
-    {
-        hand = new ArrayList<Card>();
-
-        // Draw 8 cards
-        for (int i=0; i<8; i++)
-        {
-            // Ensure the deck isn't empty
-            if (!deck.isEmpty())
-            {  
-                Card card = deck.remove(0);  // Remove the top card
-                hand.add(card);  // Add card to hand
-            }
-            else
-            {
-                System.out.println("The deck is empty, cannot draw more cards!");
-                break;  // Stop drawing if there are no more cards left in the deck
-            }
-        }
-        
-        // Error checking: Print hand
-        //System.out.println("Hand:");
-        //for (Card card : hand) {System.out.println(card);}
-
-    }
-
     public void buildGUI()
     {
 
@@ -151,33 +148,111 @@ public class Game
         gamewindow.setIconImage(new ImageIcon("icons/windowicon.png").getImage());
         
         // Window background
-        gamebg.setLayout(new BorderLayout());
+        gamebg.setLayout(null);
         gamebg.setBackground(new Color(53,101,77));
 
+        // Window Creation
+        gamewindow.setContentPane(gamebg);
+
+        buildActionPanel();
+        buildHandPanel();
+
+        gamewindow.setVisible(true);
+
+    }
+
+    public void buildActionPanel()
+    {
         // Action Panel settings
         actionpanel.setOpaque(false);
         
         // Button settings
+        //Figure out pixelated sans serif fonts later
         discardbtn.setFocusable(false);
-        discardbtn.setOpaque(true);
-        actionpanel.add(discardbtn);
+        discardbtn.setBackground(new Color(254,86,95,255));
+        discardbtn.setForeground(new Color(255,255,255,255));
+        discardbtn.setFont(new Font("sans serif",Font.PLAIN,14));
+        discardbtn.setBorder(BorderFactory.createLineBorder(Color.white, 2));
+        discardbtn.setPreferredSize(new Dimension(120,63));
+        
+        suitsortbtn.setFocusable(false);
+        suitsortbtn.setBackground(new Color(164,89,166,255));
+        suitsortbtn.setForeground(new Color(255,255,255,255));
+        suitsortbtn.setFont(new Font("sans serif",Font.PLAIN,14));
+        suitsortbtn.setBorder(BorderFactory.createLineBorder(Color.white, 2));
+        suitsortbtn.setPreferredSize(new Dimension(80,63));
 
         ranksortbtn.setFocusable(false);
-        ranksortbtn.setOpaque(true);
-        actionpanel.add(ranksortbtn);
-
-        suitsortbtn.setFocusable(false);
-        suitsortbtn.setOpaque(true);
-        actionpanel.add(suitsortbtn);
-
+        ranksortbtn.setBackground(new Color(239,191,47,255));
+        ranksortbtn.setForeground(new Color(255,255,255,255));
+        ranksortbtn.setFont(new Font("sans serif",Font.PLAIN,14));
+        ranksortbtn.setBorder(BorderFactory.createLineBorder(Color.white, 2));
+        ranksortbtn.setPreferredSize(new Dimension(80,63));
+        
         playbutton.setFocusable(false);
-        playbutton.setOpaque(true);
-        actionpanel.add(playbutton);
+        playbutton.setBackground(new Color(76,194,146,255));
+        playbutton.setForeground(new Color(255,255,255,255));
+        playbutton.setFont(new Font("sans serif",Font.PLAIN,14));
+        playbutton.setBorder(BorderFactory.createLineBorder(Color.white, 2));
+        playbutton.setPreferredSize(new Dimension(120,63));
 
-        // Window Creation
-        gamewindow.setContentPane(gamebg);
-        gamewindow.add(actionpanel, BorderLayout.SOUTH);
-        gamewindow.setVisible(true);
+
+
+        actionpanel.add(discardbtn);
+        actionpanel.add(suitsortbtn);
+        actionpanel.add(ranksortbtn);
+        actionpanel.add(playbutton);
+        actionpanel.setBounds(150, 685, 500, 75);
+
+        actionpanel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
+        gamebg.add(actionpanel);
 
     }
+
+    public void buildHandPanel()
+    {
+        handPanel = new JPanel();
+        handPanel.setOpaque(false);
+        handPanel.setLayout(new FlowLayout(FlowLayout.CENTER,handcram,0));
+        handPanel.setBounds(7, 500, 780, cardheight+20);
+        handPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
+        //Investiage later for making the handpanel look nicer
+        //handPanel.setBorder(BorderFactory.createRaisedBevelBorder());
+        gamebg.add(handPanel);
+    }
+
+    public void drawHand()
+    {
+        hand = new ArrayList<Card>();
+
+        // Draw 8 cards
+        for (int i=0; i<playerhandsize; i++)
+        {
+            // Check for empty deck
+            if (!deck.isEmpty())
+            {  
+                Card card = deck.remove(0);  // Remove the top card
+                hand.add(card);  // Add card to hand
+            }
+            else
+            {
+                System.out.println("The deck is empty");
+                break;  // Stop drawing if there are no more cards left in the deck
+            }
+        }
+
+        // Draw hand
+        for (Card card : hand)
+        {
+            ImageIcon cardImage = new ImageIcon(card.getImagePath());
+            JLabel cardLabel = new JLabel(cardImage);
+            handPanel.add(cardLabel);
+        }
+        
+        // Error checking: Print hand
+        System.out.println("Hand:");
+        for (Card card : hand) {System.out.println(card);}
+
+    }
+
 }
